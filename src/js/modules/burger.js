be1883d9acf76eec51ea683b9964514338f1af83
Denis <dean.angell@mail.ru>
2020-12-05 17:10:46 +0300

    add burger menu

---
+++ src/js/modules/burger.js
@@ -0,0 +1,23 @@
+let burger = (triggerSelector,menuSelector) =>{
+    let btn = document.querySelector(triggerSelector),
+        menu = document.querySelector(menuSelector);
+    
+    menu.style.display = 'none';
+
+    btn.addEventListener('click', ()=>{
+        if(menu.style.display == 'none' && screen.availWidth < 993){
+            menu.style.display = 'block';
+        }else{
+            menu.style.display = 'none';
+        }
+    });
+
+    window.addEventListener('resize', (e)=>{
+        if(screen.availWidth > 992){
+            menu.style.display = 'none';
+        }
+    });
+
+};
+
+export default burger;
