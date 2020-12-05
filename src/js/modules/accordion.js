b3b8b914e538119e16e79768b1f2ff14b5a21254
Denis <dean.angell@mail.ru>
2020-12-05 14:43:44 +0300

    add accordion

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,32 @@
+let accordion =(triggerSelector)=>{
+    let btns = document.querySelectorAll(triggerSelector);
+
+    btns.forEach(btn=>{
+        btn.addEventListener('click', function(){
+            btns.forEach(btn=>{
+                if(!this.classList.contains('active-style')){
+                    btn.classList.remove('active-style');
+                    btn.nextElementSibling.classList.remove('active-content');
+                    btn.nextElementSibling.style.maxHeight = 0 +'px';
+                }
+                
+            });
+
+
+            this.classList.toggle('active-style');
+            this.nextElementSibling.classList.toggle('active-content');
+
+            if(this.classList.contains('active-style')){
+                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight +80 +'px';
+            }else{
+                this.nextElementSibling.style.maxHeight = 0 +'px';
+            }
+
+        });
+    });
+
+};
+
+export default accordion;
+
+
